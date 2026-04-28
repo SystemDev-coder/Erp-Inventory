33ff08b46f739c72845e2fc921a084114d24d5ce
System Dev <systemictdevelopers@gmail.com>
2026-04-28 14:15:58 +0400

    Fix Coolify db init mount path

--- db/init/01-full-schema.sql
+++
@@ -1,5 +0,0 @@
--- Load the full schema on the very first initialization of the Postgres volume.
--- Requires `./server/sql` to be mounted into `/docker-entrypoint-initdb.d/sql`.
-
-\i '/docker-entrypoint-initdb.d/sql/Full_complete_scheme.sql'
-
